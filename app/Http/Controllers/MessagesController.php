d5fea785104bccab36a7c6a6b7ca1aa479b11610
josetanta <jose.tanta.27@unsch.edu.pe>
2020-07-03 20:04:43 -0500

    Config Mail Receiver

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -6,28 +6,24 @@ use App\Mail\MessageReceived;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
-class MessagesController extends Controller
-{
+class MessagesController extends Controller {
 
-	public function show()
-	{
+	public function show() {
 		return view('contact');
 	}
 
-	public function send()
-	{
+	public function send() {
 		$message = request()->validate([
 			'name' => 'required',
 			'email' => 'required|email',
 			'asunt' => 'required',
-			'message' => 'required|min:5'
+			'message' => 'required|min:5',
 		]);
 
-		Mail::to('jose.tanta.27@unsch.edu.pe')
-				->queue(new MessageReceived($message));
+		Mail::to(env('MAIL_ADMIN'))
+			->queue(new MessageReceived($message));
 		// return new MessageReceived($message);
 		return "Formulario Correcto Y Mensaje Enviado";
 	}
-
 
 }
